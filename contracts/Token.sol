5faa1613597b207bc42e97796bf50868db948615
LucasPerezDev <LucasPerezDev@gmail.com>
2022-07-18 03:15:30 -0400

    Added ability to check balance within the token's smart contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,5 +5,25 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "My Token";
+    string public name;
+    string public symbol;
+    // Add Decimals
+    uint256 public decimals = 18;
+    // Add Total supply
+    uint256 public totalSupply;
+    // Track balances
+    mapping(address => uint256) public balanceOf;
+
+    // Send tokens
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
 }
--- test/Token.js
+++ test/Token.js
@@ -1,14 +1,48 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 describe("Token contract", () => {
-  it("Has a name", async () => {
-    //Fetch token from Blockchain
+  let token, 
+      accounts, 
+      deployer
+
+  beforeEach(async () => {
+    // Code goes here...
     const Token = await ethers.getContractFactory("Token");
-    let token = await Token.deploy();
-    //Read token name
-    const name = await token.name();
-    //Check that name is correct
-    expect(name).to.equal("My Token");
+    token = await Token.deploy("Riderian", "RIDE", "1000000");
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+  });
+
+  describe("Deployment", () => {
+    const name = "Riderian";
+    const symbol = "RIDE";
+    const decimals = "18";
+    const totalSupply = tokens("1000000");
+
+
+    it("Has correct name", async () => {
+      //Check that name is correct
+      expect(await token.name()).to.equal(name);
+    });
+    it("Has correct symbol", async () => {
+      //Check that name is correct
+      expect(await token.symbol()).to.equal(symbol);
+    });
+    it("Has correct decimals", async () => {
+      //Check that name is correct
+      expect(await token.decimals()).to.equal(decimals);
+    });
+    it("Has correct total supply", async () => {
+      //Check that name is correct
+      expect(await token.totalSupply()).to.equal(totalSupply);
+    });
+    it("assigns total supply to deployer", async () => {
+      //Check that name is correct
+      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
+    });
   });
 });
