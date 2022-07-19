77fb2cf6b9c73f9430a76e0b6c602fe4bfafb732
LucasPerezDev <LucasPerezDev@gmail.com>
2022-07-18 23:23:13 -0400

    Added withdrawal functionality and testing as well as checking balances testing.

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-
 import "hardhat/console.sol";
 import "./Token.sol";
-
 
 contract Exchange {
     address public feeAccount;
@@ -12,13 +10,20 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(
+        address token,
+        address user,
+        uint256 amount,
+        uint256 balance
+    );
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
     }
-//Deposit tokens
-    function depositToken(address _token, uint256 _amount) public{
+
+    //Deposit tokens
+    function depositToken(address _token, uint256 _amount) public {
         //Transfer tokens to exchange
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
         //Update balance
@@ -27,24 +32,33 @@ contract Exchange {
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
-//Withdraw tokens
+    //Withdraw tokens
+    function withdrawToken(address _token, uint256 _amount) public {
+        //Ensure user has enough tokens to withdraw
+        require(tokens[_token][msg.sender] >= _amount);
+        //Transfer tokens to the user
+        Token(_token).transfer(msg.sender, _amount);
+        //Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+        //Emit an event
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
 
-//Check balances
+    //Check balances
     function balanceOf(address _token, address _user)
         public
         view
-        returns (uint256) 
+        returns (uint256)
     {
         return tokens[_token][_user];
     }
-//Make orders
+    //Make orders
 
-//Cancel orders
+    //Cancel orders
 
-//Fill orders
+    //Fill orders
 
-//Charge fees
+    //Charge fees
 
-//Track fee account
-
+    //Track fee account
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -84,4 +84,80 @@ describe("Exchange", () => {
       });
     });
   });
+
+  describe("Withdrawing tokens", () => {
+    let transaction, result;
+    let amount = tokens(10);
+
+    describe("Success", async () => {
+      beforeEach(async () => {
+        //Approve tokens
+        transaction = await token1
+          .connect(user1)
+          .approve(exchange.address, amount);
+        result = await transaction.wait();
+        //Deposit tokens
+        transaction = await exchange
+          .connect(user1)
+          .depositToken(token1.address, amount);
+        result = await transaction.wait();
+
+        //Now withdraw tokens
+        transaction = await exchange
+          .connect(user1)
+          .withdrawToken(token1.address, amount);
+        result = await transaction.wait();
+      });
+
+      it("Withdraws the token funds", async () => {
+        expect(await token1.balanceOf(exchange.address)).to.equal(0);
+        expect(await exchange.tokens(token1.address, user1.address)).to.equal(
+          0
+        );
+        expect(
+          await exchange.balanceOf(token1.address, user1.address)
+        ).to.equal(0);
+      });
+      it("Emits a withdraw event", async () => {
+        const event = result.events[1];
+        expect(event.event).to.equal("Withdraw");
+
+        const args = event.args;
+        expect(args.token).to.equal(token1.address);
+        expect(args.user).to.equal(user1.address);
+        expect(args.amount).to.equal(amount);
+        expect(args.balance).to.equal(0);
+      });
+    });
+
+    describe("Failure", async () => {
+      it("Fails for insufficient balance", async () => {
+        await expect(
+          exchange.connect(user1).withdrawToken(token1.address, amount)
+        ).to.be.reverted;
+      });
+    });
+  });
+
+  describe("Checking balances", () => {
+    let transaction, result;
+    let amount = tokens(1);
+
+    beforeEach(async () => {
+        //Approve tokens
+        transaction = await token1
+          .connect(user1)
+          .approve(exchange.address, amount);
+        result = await transaction.wait();
+        //Deposit tokens
+        transaction = await exchange
+          .connect(user1)
+          .depositToken(token1.address, amount);
+        result = await transaction.wait();
+    })
+      it("Returns user balance", async () => {
+        expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount);
+      });
+    });
 });
+
