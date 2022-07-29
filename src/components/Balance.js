f9aa371b4bec99067be1de3d119809e12d851a19
LucasPerezDev <LucasPerezDev@gmail.com>
2022-07-29 01:39:41 -0400

    Added the functional components of user withdrawals. Both tokens can be deposited and withdrawn to and from the exchange.

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -76,6 +76,32 @@ const Balance = () => {
     }
   };
 
+  const withdrawHandler = (e, token) => {
+    e.preventDefault();
+
+    if (token.address === tokens[0].address) {
+      transferTokens(
+        provider,
+        exchange,
+        "Withdraw",
+        token,
+        token1TransferAmount,
+        dispatch
+      );
+      setToken1TransferAmount(0);
+    } else {
+      transferTokens(
+        provider,
+        exchange,
+        "Withdraw",
+        token,
+        token2TransferAmount,
+        dispatch
+      );
+      setToken2TransferAmount(0);
+    }
+  };
+
   useEffect(() => {
     if (exchange && tokens[0] && tokens[1] && account) {
       loadBalances(exchange, tokens, account, dispatch);
@@ -122,7 +148,13 @@ const Balance = () => {
           </p>
         </div>
 
-        <form onSubmit={(e) => depositHandler(e, tokens[0])}>
+        <form
+          onSubmit={
+            isDeposit
+              ? (e) => depositHandler(e, tokens[0])
+              : (e) => withdrawHandler(e, tokens[0])
+          }
+        >
           <label htmlFor="token0"></label>
           <input
             type="text"
@@ -160,7 +192,13 @@ const Balance = () => {
           <br />
           {exchangeBalances && exchangeBalances[1]}
         </p>
-        <form onSubmit={(e) => depositHandler(e, tokens[1])}>
+        <form
+          onSubmit={
+            isDeposit
+              ? (e) => depositHandler(e, tokens[1])
+              : (e) => withdrawHandler(e, tokens[1])
+          }
+        >
           <label htmlFor="token1"></label>
           <input
             type="text"
--- src/store/interactions.js
+++ src/store/interactions.js
@@ -1,7 +1,6 @@
 import { ethers } from "ethers";
 import TOKEN_ABI from "../abis/Token.json";
 import EXCHANGE_ABI from "../abis/Exchange.json";
-import { exchange } from "./reducers";
 
 export const loadProvider = (dispatch) => {
   const connection = new ethers.providers.Web3Provider(window.ethereum);
@@ -57,6 +56,9 @@ export const subscribeToEvents = (exchange, dispatch) => {
   exchange.on("Deposit", (token, user, amount, balance, event) => {
     dispatch({ type: "TRANSFER_SUCCESS", event });
   });
+  exchange.on("Withdraw", (token, user, amount, balance, event) => {
+    dispatch({ type: "TRANSFER_SUCCESS", event });
+  });
 };
 //Load user balances (wallet and exchange balances)
 
@@ -100,14 +102,20 @@ export const transferTokens = async (
     const signer = await provider.getSigner();
     const amountToTransfer = ethers.utils.parseUnits(amount.toString(), 18);
 
-    transaction = await token
-      .connect(signer)
-      .approve(exchange.address, amountToTransfer);
-    await transaction.wait();
-    transaction = await exchange
-      .connect(signer)
-      .depositToken(token.address, amountToTransfer);
-    await transaction.wait();
+    if (transferType === "Deposit") {
+      transaction = await token
+        .connect(signer)
+        .approve(exchange.address, amountToTransfer);
+      await transaction.wait();
+      transaction = await exchange
+        .connect(signer)
+        .depositToken(token.address, amountToTransfer);
+    } else {
+      transaction = await exchange
+        .connect(signer)
+        .withdrawToken(token.address, amountToTransfer);
+      await transaction.wait();
+    }
   } catch (error) {
     dispatch({ type: "TRANSFER_FAIL" });
   }
