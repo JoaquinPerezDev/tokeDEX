8667737a4cab898c4257721c6137fbd16237449c
LucasPerezDev <LucasPerezDev@gmail.com>
2022-08-01 03:13:02 -0400

    Running into some errors while deploying to Kovan network

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ const { task } = require("hardhat/config");
 
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -23,5 +24,9 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     localhost: {},
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(","),
+    },
   },
 };
